e8ce67142291a6abceed8abb0a57bbef68b3dc7c
CarlosEmilioAlcantara <agq.xyz@gmail.com>
2023-07-17 15:26:36 +0800

    bruh

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
+    screens: {
+      'sm': '0px',
+      
+      'lsm': '650px',
+      // => @media (min-width: 640px) { ... }
+
+      'lmd': '770px',
+
+      'md': '950px',
+
+      'lg': '1000px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1200px',
+      // => @media (min-width: 1280px) { ... }
+    },
     colors: {
       'light': '#fff',
       'dark': '#333',
@@ -15,6 +31,7 @@ module.exports = {
       'button': '#0066b8',
       'button-hover': '#005ba4',
       'burger-hover': '#0072be',
+      'search-bar': 'rgba(255,255,255,.08)'
     },
     fontFamily: {
       'body': ['"Noto Sans", sans-serif'],
